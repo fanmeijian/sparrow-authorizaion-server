d00b1ef1e7dc3d94a52279469f01d9df1b4ccd0d
fanmeijian <fanmeijian@aliyun.com>
2020-12-12 12:30:50 +0800

    增加对eureka的支持

--- src/main/java/cn/youweisoft/authorization/server/AuthorizationServerApplication.java
+++ src/main/java/cn/youweisoft/authorization/server/AuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package cn.youweisoft.authorization.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthorizationServerApplication {
     public static void main(String[] args) {
