95d646d15930b7306574946efef102e225b53f3b
fansword <fansword@localhost>
2021-07-02 20:26:22 +0800

    解决迁移到苹果平台错误

--- src/main/java/cn/youweisoft/authorization/server/MyUserDetailsService.java
+++ src/main/java/cn/youweisoft/authorization/server/MyUserDetailsService.java
@@ -3,12 +3,14 @@ package cn.youweisoft.authorization.server;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import cn.youweisoft.authorization.server.model.SwdAuthority;
@@ -18,12 +20,16 @@ import cn.youweisoft.authorization.server.repository.UserRepository;
 
 @Component
 public class MyUserDetailsService implements UserDetailsService {
+	public static Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
+	
 	@Autowired
 	UserRepository userRepository;
 
 	@Autowired
 	AuthorityRepository authorityRepository;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -31,6 +37,8 @@ public class MyUserDetailsService implements UserDetailsService {
 //		Assert.notNull(swdUser, "can not found user" + username);
 //		Logger.getLogger(this.toString()).info("----------" + username + swdUser.getPassword() + swdUser.getPassword());
 		List<SwdAuthority> authorities = new ArrayList<SwdAuthority>();
+		logger.info("{} {}",swdUser.getPassword(), passwordEncoder.encode("password"));
+		
 		
 		authorities.addAll(swdUser.getSwdAuthorities());
 		swdUser.getSwdSysroles().forEach(o -> {
--- src/main/java/cn/youweisoft/authorization/server/WebSecurityConfig.java
+++ src/main/java/cn/youweisoft/authorization/server/WebSecurityConfig.java
@@ -1,20 +1,17 @@
 package cn.youweisoft.authorization.server;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-
-
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	@Autowired
+//	MyUserDetailsService userDetailsService;
+//
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.userDetailsService(userDetailsService);
 //	}
-
+//
 //	@Override
 //	@Bean
 //	public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -27,14 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
 //        this.passwordEncoder = passwordEncoder;
 //    }
-    
+//    
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //    	http.oauth2Login(oauth2Login->
 //    	oauth2Login.userInfoEndpoint()
 //    	);
 //    }
-	
+//	
 //	@Bean
 //	public ClientDetailsService clientDetailsService() {
 //		return new MyClientDetailsService();
@@ -44,4 +41,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    public UserDetailsService userDetailsService() {
 //        return new MyUserDetailsService();
 //    }
-}
+//}
